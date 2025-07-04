d20c5f715af93aa14b66793e7aace529755a87c3
cyber-Rahul1 <rahulsatvik@gmail.com>
2025-07-04 15:44:57 +0530

    fbgfh

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,14 +1,7 @@
 import multer from 'multer';
 
 
-const storage = multer.memoryStorage({
-    destination : function (req, file, cb) {
-        cb(null, './public')
-    },
-    filename : function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage });
 
